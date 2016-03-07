e52dcfab3c93792ea02fe3d47a290756e1d0c976
Dylan Hurd <dylan.hurd@clever.com>
2016-03-07 19:45:42 +0000

    actually works now

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# red-herring
-This is not the repo you're looking for.
+# Alpha Notes
+This is a chrome extension designed to let you save notes for a webpage.
+
+## How to run
+- Clone the repo.
+- (In Chrome) Go to Extensions. Check developer mode in the top right.
+- Click "Load unpacked extension."
+- Select this folder.
+- That's it!
--- index.js
+++ index.js
@@ -3,19 +3,25 @@
 // the notes done. The code the students will see needs to be well
 // documented, and well-abstracted so that they can quickly understand
 // what's going on and fill in a function or two
-var sidebar = "<div class='annotate-sidebar'><div class='annotate-header'><h2>Notes</h2></div><div class='annotate-list'></div><input type=submit class='annotate-save' value='Save'><input class='annotate-footer' placeholder='Type notes here!'></div>";
+var sidebar = "<div class='annotate-sidebar'><div class='annotate-header'>Notes</div><div class='annotate-list'></div><input type=submit class='annotate-save' value='Save'><input class='annotate-footer' placeholder='Type notes here!'></div>";
+
+var toggle = "<div class='annotate-toggle'>X</div>";
 
 var noteTemplate = "<div class='annotate-note'></div>";
 
-// Saves a note to storage
-var saveNote = function(text, callback) {
+var getNotes = function(callback) {
 	chrome.storage.sync.get('list', function(storage) {
 		var list = storage.list;
-		if (Object.keys(list).length === 0) {
+		if (!list || Object.keys(list).length === 0) {
 			list = [];
 		}
-		console.log("list: ");
-		console.log(list);
+		callback(list);
+	});
+};
+
+// Saves a note to storage
+var saveNote = function(text, callback) {
+	getNotes(function(list) {
 		list.push({
 			text: text
 		});
@@ -25,6 +31,14 @@ var saveNote = function(text, callback) {
 	});
 };
 
+var renderNotes = function(list) {
+	$('.annotate-note').remove();
+	list.forEach(function(note){
+		var noteView = $.parseHTML(noteTemplate);
+		$(noteView).text(note.text);
+		$('.annotate-list').append(noteView);
+	});
+};
 
 // listen for changes to storage and render notes. Because this is a listener
 // saveNote will also result in the note being shown
@@ -33,22 +47,27 @@ chrome.storage.onChanged.addListener(function(changes, namespace) {
 	if (!listChange) {
 		return;
 	}
-	var list = listChange.newValue;
-	list.forEach(function(note){
-		var noteView = $.parseHTML(noteTemplate);
-		noteView.innerHTML = note.text; // this is broken still. i am bad at jquery.
-		$('.annotate-list').append(noteView);
-	});
+	renderNotes(listChange.newValue);
 });
 
 $(document).ready(function() {
 	$('body').append(sidebar);
+
+	// Button to show/hide notes
+	$('body').append(toggle);
+	$('.annotate-toggle').click(function() {
+		$('.annotate-sidebar').toggle();
+		$('.annotate-toggle').toggleClass('hidden');
+	});
+
+	// Notes functionality
 	$('.annotate-save').click(function() {
 		saveNote($('.annotate-footer').val(), function() {
 			console.log("saved");
 		});
 	});
-// Experiment to fix rest of page
+	getNotes(renderNotes);
+// Experiment to move rest of page over - this is turning out to be hard
 //	$('body').wrapInner("<div class='annotate-wrap' />");
 //	$('.annotate-wrap').css('width','70%');
 });
